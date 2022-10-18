19fdd99679cb11677fc99e16cbf24f4860f8ad9c
John <johnreysecoya@gmail.com>
2022-10-18 08:12:49 +0800

    add color changer

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 let grid_number = 0;
 let container = document.querySelector(".main-container");
+
 let total_number_boxes = 0;
 let grid_btn = document.querySelector(".set-grid");
 let clear_btn = document.querySelector(".clear-grid");
@@ -8,8 +9,15 @@ let clear_btn = document.querySelector(".clear-grid");
 function set_grid_boxes() {
   for (let i = 1; i <= total_number_boxes; i++) {
     let box = document.createElement("div");
-    box.classList.add("inside-box");
+    box.classList.add("inside-box", "hover-cover-background-color");
     container.appendChild(box);
+  }
+}
+
+function set_background_color() {
+  let background = document.querySelector(".inside-box");
+  for (let element of container.children) {
+    element.style.backgroundColor = `${color}`;
   }
 }
 
@@ -31,7 +39,7 @@ function add_hover_class() {
 }
 
 function hover_effect() {
-  this.classList.add("inside-box-hover");
+  this.classList.remove("hover-cover-background-color");
 }
 
 function remove_container_div() {
@@ -41,7 +49,7 @@ function remove_container_div() {
 }
 
 function remove_hover_effect(element) {
-  element.classList.remove("inside-box-hover");
+  element.classList.add("hover-cover-background-color");
 }
 
 grid_btn.addEventListener("click", () => {
@@ -49,6 +57,7 @@ grid_btn.addEventListener("click", () => {
   grid_number = parseInt(prompt("Inter grid number"));
   total_number_boxes = grid_number * grid_number;
   set_grid_boxes();
+  set_background_color();
   set_grid_columns_rows();
   add_hover_class();
 });
@@ -57,4 +66,16 @@ clear_btn.addEventListener("click", () => {
   for (let element of container.children) {
     remove_hover_effect(element);
   }
+});
+
+let color_picker = document.querySelector("#colorpicker");
+
+let color = color_picker.value;
+
+color_picker.addEventListener("change", (e) => {
+  color = color_picker.value;
+  for (let element of container.children) {
+    remove_hover_effect(element);
+  }
+  set_background_color();
 });
